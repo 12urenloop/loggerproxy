a04d7205ca110fb0b4874d370f94d8b48f68f14c
Robbe Van Herck <robbevanherck1@gmail.com>
2019-04-04 17:58:25 +0200

    Added print to log

--- loggingproxy.py
+++ loggingproxy.py
@@ -15,7 +15,7 @@ for key, ip_port in servers.items():
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         ip, port = ip_port.split(":")
         sock.connect((ip, int(port)))
-        connections[key] = sock
+        connections[key.lower()] = sock
     except:
         print(f"Could not connect to {ip_port}")
 
@@ -25,20 +25,23 @@ proxy_socket.listen(10)
 
 def log(msg, prefix="MSG"):
     global lck
+    full_mesg = f"[{prefix}] {msg}\n"
     lck.acquire(timeout=-1)
     with open(logging_conf["logfile_path"], "a") as f:
-        f.write(f"[{prefix}] {msg}\n")
+        f.write(full_mesg)
     lck.release()
+    print(full_mesg, end="")
 
 def handle_connection(clientsocket, address):
+    log(f"New connection from {address}")
     buff = ""
     while clientsocket:
         data = clientsocket.recv(1).decode()
         if data == "\n":
             buff += "\n"
             buff_split = buff.split(",", 1)
-            if len(buff_split) == 2 and buff_split[0] in connections:
-                connections[buff_split[0]].sendall(buff_split[1].encode())
+            if len(buff_split) == 2 and buff_split[0].lower() in connections:
+                connections[buff_split[0].lower()].sendall(buff_split[1].encode())
                 log(f"Sending \"{buff_split[1][:-1]}\" from {address} to {buff_split[0]}")
             else:
                 log(f"Got \"{buff[:-1]}\" from {address}, don't know what to do with that", prefix="ERR")
@@ -49,6 +52,6 @@ try:
     while True:
         _thread.start_new_thread(handle_connection, proxy_socket.accept())
 finally:
-    proxy_socket.close()
+    print(proxy_socket.close())
     for sock in connections.values():
         sock.close()
