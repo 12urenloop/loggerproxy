49198bdc4943a6241b2176280097fe67a85dcbbd
abeforgit <arnebertrand@gmail.com>
2019-04-27 20:00:16 +0200

    rewrite 2

--- server.py
+++ server.py
@@ -1,113 +1,97 @@
+from typing import Tuple, Type, List
+from socketserver import ThreadingTCPServer, ThreadingMixIn, BaseRequestHandler
+from time import time
 import socket
-from time import process_time
-from enum import Enum, auto
-from typing import Tuple, List, Dict
-
-BACKLOG: int = 10
+import threading
 RECONNECT_DELAY = 1
-LISTENER_TIMEOUT = 1
-Listener = Tuple[str, int]
+
+def log(msg, prefix="MSG"):
+    print(f"{prefix}: {msg}")
 
 
 class Listener:
     def __init__(self, host: str, port: int):
         self.host = host
         self.port = port
-        self.last_update = 0
-        self.status: SocketStatus = SocketStatus.INACTIVE
+        self.last_updated: float = 0
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.settimeout(LISTENER_TIMEOUT)
+
+    def send(self, msg: bytes):
+        self.socket.sendall(msg)
 
     def connect(self):
         self.socket.connect((self.host, self.port))
-
-    def send(self, msg):
-        self.socket.sendall(msg)
-        self.last_update = process_time()
-
-    def close(self):
-        self.socket.shutdown(socket.SHUT_RD)
-        self.socket.close()
-
-    def __str__(self):
-        return f"{self.host}:{self.port}"
-
-
-class SocketStatus(Enum):
-    ACTIVE = auto()
-    INACTIVE = auto()
-
-
-class Server:
-    def __init__(self, listeners: List[Listener]):
-        self.insock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.listeners = listeners
-        self.reconnect_queue: List[Listener] = []
-        self.last_reconnect: float = 0.0
-        self.insock.settimeout(RECONNECT_DELAY)
-        for listener in self.listeners:
-            self.reconnect_queue.append(listener)
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        for listener in self.listeners:
-            listener.close()
-        self.insock.shutdown(socket.SHUT_RD)
-        self.insock.close()
+        self.socket.close()
 
-    def log(self, msg, prefix="MSG"):
-        print(prefix, msg)
+    def __str__(self):
+        return f"{self.host}:{self.port}"
+    def __repr__(self):
+        return self.__str__()
 
-    def handle(self, clientsocket: socket.socket, address: str):
 
-        self.log(f"New connection from {address}")
+class Handler(BaseRequestHandler):
+    def setup(self):
+        self.server.reconnect_all()
+        self.server.reconnect_queue.clear()
+
+    def handle(self):
         buff = ""
         cur = ""
         while cur != "\n":
-            cur = clientsocket.recv(1).decode()
+            cur = self.request.recv(1).decode()
             buff += cur
         buff += cur
         buff_split = buff.split(",", 1)
         if len(buff_split) == 2:
-            self.send_all(buff_split[0].lower(), buff_split[1].encode())
+            msg = buff_split[1].encode()
+            for listener in self.server.listeners:
+                try:
+                    listener.send(msg)
+                    log(f"sending {msg} from {self.client_address} to {listener}")
+                except OSError:
+                    log(f"Failed to send to {listener}")
+                    self.server.reconnect_queue.append(listener)
+
         else:
-            self.log(f"Got \"{buff[:-1]}\" from {address}, don't know what to do with that", prefix="ERR")
+            log(f"Got {buff[:-1]} from {self.client_address}, don't know what to do with it")
 
-    def send_all(self, from_address: str, msg: bytes):
-        for listener in self.listeners:
-            try:
-                self.log(f"Sending {msg} coming from {from_address} to {listener}")
-                listener.send(msg)
-            except OSError:
-                self.reconnect_queue.append(listener)
 
-    def listen(self, port: int):
-        self.insock.bind(("localhost", port))
-        self.insock.listen(BACKLOG)
+class Server(ThreadingTCPServer):
+
+    def __init__(self, address: Tuple[str, int], handler: Type[BaseRequestHandler], listeners: List[Listener]):
+        super().__init__(address, handler)
+        self.address_family = socket.SOCK_STREAM
+        self.listeners = listeners
+        self.reconnect_queue = listeners[:]
+        self.last_reconnect = -RECONNECT_DELAY
         self.reconnect_all()
-        while True:
-            try:
-                (clientsocket, address) = self.insock.accept()
-                self.handle(clientsocket, address)
-            except socket.timeout:
-                self.reconnect_all()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        super().__exit__(exc_type, exc_val, exc_tb)
+        for listener in self.listeners:
+            listener.__exit__()
+        self.shutdown()
 
     def reconnect_all(self):
-        self.last_reconnect = process_time()
-        for _ in range(len(self.reconnect_queue)):
-            listener = self.reconnect_queue.pop()
-            self.log(f"Connecting to {listener}")
-            try:
-                listener.connect()
-                self.log(f"Succesfully connected to {listener}")
-            except OSError:
-                self.log(f"Could not connect to {listener}", "ERR")
-                self.reconnect_queue.append(listener)
+        if time() - self.last_reconnect > RECONNECT_DELAY:
+            self.last_reconnect = time()
+            for _ in range(len(self.reconnect_queue)):
+                listener = self.reconnect_queue.pop(0)
+                try:
+                    log(f"Connecting to {listener}")
+                    listener.connect()
+                except OSError:
+                    log(f"Could not connect to {listener}, will retry later")
+                    self.reconnect_queue.append(listener)
 
 
+HOST, PORT = "localhost", 12345
 l1 = Listener("0.0.0.0", 5000)
-
-with Server([l1]) as server:
-    server.listen(12345)
+l2 = Listener("0.0.0.0", 5001)
+with Server((HOST, PORT), Handler, [l1, l2]) as server:
+    server.serve_forever()
